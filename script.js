9e01b00d6d7a1982c5d00f5c0292994dc6cac949
awduley <duleyalaska@gmail.com>
2025-05-01 14:25:58 -0700

    Further the library logic
    I'm running into a few snags but I'll get it worked
    out

--- script.js
+++ script.js
@@ -1,36 +1,90 @@
 const display = document.querySelector('.display');
+const add = document.querySelector('.add');
+// const remove = document.querySelector('.remove');
+const dialog = document.querySelector('.dialog');
+const submit = document.querySelector('.submit');
+const cancel = document.querySelector('.cancel');
+const form = document.querySelector('form');
 
-const myLibrary = [
-  {
-    name: 'Halloweenie',
-    author: 'Sam Mam',
-    yearPublished: 1965,
-    id: self.crypto.randomUUID()
-  },
-  {
-    name: 'The Lobster Man',
-    author: 'Martin Spleash',
-    yearPublished: 1889,
-    id: self.crypto.randomUUID()
-  },
-];
+const myLibrary = [];
 
-function Book(name, author, yearPublished) {
-  this.name = name;
+function Book(title, author, yearPublished) {
+  this.title = title;
   this.author = author;
   this.yearPublished = yearPublished;
   this.id = self.crypto.randomUUID();
 }
 
-function addBookToLibrary(n, a, y) {
-  myLibrary.push(new Book(n, a, y));
+function addBookToLibrary(t, a, y) {
+  myLibrary.push(new Book(t, a, y));
 }
-
-addBookToLibrary('The Great Pizza', 'Andrew', 2025);
-addBookToLibrary('The Great Pizza', 'Andrew', 2025);
 
 myLibrary.forEach(item => {
   const newDiv = document.createElement('div');
-  newDiv.textContent = JSON.stringify(item);
+
+  const title = document.createElement('h3');
+  title.textContent = item.title;
+  newDiv.appendChild(title);
+
+  const author = document.createElement('p');
+  author.textContent = item.author;
+  newDiv.appendChild(author);
+
+  const yearPublished = document.createElement('p');
+  yearPublished.textContent = item.yearPublished;
+  newDiv.appendChild(yearPublished);
+
+  const id = document.createElement('p');
+  id.textContent = item.id;
+  newDiv.appendChild(id);
+
   display.appendChild(newDiv);
 });
+
+add.addEventListener('click', () => {
+  dialog.classList.add('show');
+});
+
+// remove.addEventListener('click', () => {
+//   // Do something
+// })
+
+cancel.addEventListener('click', (e) => {
+  dialog.classList.remove('show');
+  e.preventDefault();
+});
+
+form.addEventListener('submit', (e) => {
+  const formData = new FormData(form);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const yearPublished = formData.get('year-published');
+
+  addBookToLibrary(title, author, yearPublished);
+
+  // myLibrary.forEach(item => {
+  //   const newDiv = document.createElement('div');
+  
+  //   const title = document.createElement('h3');
+  //   title.textContent = item.title;
+  //   newDiv.appendChild(title);
+  
+  //   const author = document.createElement('p');
+  //   author.textContent = item.author;
+  //   newDiv.appendChild(author);
+  
+  //   const yearPublished = document.createElement('p');
+  //   yearPublished.textContent = item.yearPublished;
+  //   newDiv.appendChild(yearPublished);
+  
+  //   const id = document.createElement('p');
+  //   id.textContent = item.id;
+  //   newDiv.appendChild(id);
+  
+  //   display.appendChild(newDiv);
+  // });
+
+  e.preventDefault();
+
+  form.reset();
+})
